2d6209c6534b4fa53b4bebe490efd6157d23d858
sheler1988 <93482890+sheler1988@users.noreply.github.com>
2024-10-12 15:31:39 +0330

    1

---
+++ ResizeImage/ResizeImage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ResizeImage
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
---
+++ ResizeImage/ResizeImage/Services/ImageTools.cs
@@ -0,0 +1,49 @@
+﻿
+// این کد به طور کلی یک روش مؤثر برای تغییر اندازه تصاویر با کیفیت بالا در سی‌شارپ ارائه می‌دهد
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ResizeImage.Services;
+
+public class ImageTools // ImageTools کلاس
+{
+	// متدی که یک تصویر و عرض هدف را به عنوان ورودی دریافت می‌کند
+	public Image ResizeImage(Image original, int targetWidth)
+	{
+		// محاسبه ابعاد جدید تصویر
+		//درصد تغییر اندازه محاسبه می‌شود. سپس عرض  و ارتفاع  جدید تصویر با حفظ نسبت ابعاد اصلی تصویر محاسبه می‌شوند
+		var percent = (double)original.Width / targetWidth;
+		var destWidth = (int)(original.Width / percent);
+		var destHeight = (int)(original.Height / percent);
+
+		// ایجاد یک تصویر جدید با اندازه تغییر یافته
+		// یک شیء جدید از نوع بیت مپ با اندازه جدید ایجاد می‌کند و یک شیء کرافیکس برای رسم در آن ایجاد می‌کند
+		var b = new Bitmap(destWidth, destHeight);
+		var g = Graphics.FromImage(b);
+		try
+		{
+			// تنظیمات کرافیکی برای بهبود کیفیت تغییر اندازه
+			// این تنظیمات کرافیکی برای اطمینان از کیفیت بالا هنکام تغییر اندازه تصویر استفاده می‌شود
+			g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			g.SmoothingMode = SmoothingMode.HighQuality;
+			g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+			g.CompositingQuality = CompositingQuality.HighQuality;
+
+			// رسم تصویر تغییر اندازه یافته
+			// تصویر اصلی با اندازه جدید رسم می‌شود
+			g.DrawImage(original, 0, 0, destWidth, destHeight);
+		}
+		finally
+		{
+			g.Dispose();
+		}
+		// برکشت تصویر تغییر اندازه یافته
+		return b;
+	}
+}
